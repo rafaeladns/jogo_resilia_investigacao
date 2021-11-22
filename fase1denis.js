5293161fb03d97497f616a4f3c66be29f23b4960
Rafaela Dias <93568221+rafaeladns@users.noreply.github.com>
2021-11-22 15:35:34 -0300

    Algumas pastas javascript adicionadas

---
+++ fase1denis.js
@@ -0,0 +1,16 @@
+function responder(){
+    var resposta = prompt
+    while(resposta !=1 || resposta != 2){
+    resposta = prompt ("Digite uma opção")
+    if(resposta == 1){
+    window.location.href = "./gameover1denis.html"
+    break
+    }else if(resposta == 2){
+    window.location.href = "./fase2denis.html"
+    break
+    }else{
+    alert("Digite 1 ou 2")
+    break
+    }
+}
+}
